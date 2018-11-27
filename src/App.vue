b6e3b14a6e8791e1c03d0da604d0528b471d6963
yuandunggan <536584936@qq.com>
2018-11-27 20:15:26 +0800

    第一次git提交

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+      <header :class="{red:isred}">
+          <div v-html="msg"></div>
+      </header>
+      <button @click="Btn()">按钮</button>
+      <div v-if="true">
+        我是if 的 true
+      </div>
+      <form action="">
+        <input type="text" name="" id="" v-model="text">
+        <input type="checkbox" value="1" v-model="checkboxList">
+        <label for="">1</label>
+        <input type="checkbox" value="2" v-model="checkboxList">
+        <label for="">2</label>
+        <input type="checkbox" value="3" v-model="checkboxList">
+        <label for="">3</label>
+      </form>
+      <div>{{text}}</div>
+      <div>checkboxList的值:{{checkboxList}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: this.shouList(),
+      isred:true,
+      text:"",
+      checkboxList:[]
+    };
+  },
+  methods: {
+    shouList(){
+      let str="";
+      for(var i=0;i<6;i++){
+        str+="<h1><span></span></h1>";
+      }
+      return str;
+    },
+    Btn(){
+      this.isred=!(this.isred)
+    }
+  }
+};
+</script>
+
+<style>
+header {
+  width: 100vw;
+  height: 85px;
+  background-color: goldenrod;
+}
+.red{
+  background-color: steelblue;
+}
+</style>
